d2839d54fc9c45894d2b395075bc220e7ece3c52
TeenagesWitch <hananmuhammadkemal@gmail.com>
2023-11-03 14:05:31 +0700

    Add database configuration and refactor migrations

--- projects/server/config/config.js
+++ projects/server/config/config.js
@@ -1,10 +1,14 @@
+require("dotenv").config(
+  { path: process.env.NODE_ENV === "test" ? ".env.test" : ".env" }
+);
+
 module.exports = {
-  "development": {
-    "username": "root",
-    "password": "fauza2023",
-    "database": "JCWD250401",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_BASE,
+    host: process.env.DB_HOST,
+    dialect: "mysql",
   },
   test: {
     username: "root",
