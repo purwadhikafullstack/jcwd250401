497584876d974cbc18f31ef7c6e098b7ca16e58c
anggaratriputra <anggaratriputra@gmail.com>
2023-11-06 13:33:25 +0700

    add router, controller, nodemailer lib, register

---
+++ projects/server/src/index copy.js
@@ -0,0 +1,88 @@
+// require("dotenv/config");
+// const express = require("express");
+// const cors = require("cors");
+// const { join } = require("path");
+// const PORT = 8000;
+// const app = express();
+
+// app.use(express.json());
+
+// app.use(
+//   cors({
+//     origin: [
+//       process.env.WHITELISTED_DOMAIN &&
+//         process.env.WHITELISTED_DOMAIN.split(","),
+//     ],
+//   })
+// );
+
+// app.use(express.json());
+
+//#region API ROUTES
+
+// ===========================
+// NOTE : Add your router here
+const authRouter = require("./routes/auth");
+
+// routing
+app.use("/auth", authRouter);
+
+
+// ===========================
+
+// not found
+
+app.use((req, res) => {
+  console.log(`404: ${req.url}`);
+  res.status(404).json({
+    msg: "NOT FOUND",
+  });
+});
+
+app.use((err, req, res) => {
+  console.log(`500: ${res.url}`);
+  console.log(err);
+  res.status(500).json({
+    msg: "FATAL ERROR",
+    err,
+  });
+});
+
+// app.use((req, res, next) => {
+//   if (req.path.includes("/api/")) {
+//     res.status(404).send("Not found !");
+//   } else {
+//     next();
+//   }
+// });
+
+// // error
+// app.use((err, req, res, next) => {
+//   if (req.path.includes("/api/")) {
+//     console.error("Error : ", err.stack);
+//     res.status(500).send("Error !");
+//   } else {
+//     next();
+//   }
+// });
+
+//#endregion
+
+// //#region CLIENT
+// const clientPath = "../../client/build";
+// app.use(express.static(join(__dirname, clientPath)));
+
+// // Serve the HTML page
+// app.get("*", (req, res) => {
+//   res.sendFile(join(__dirname, clientPath, "index.html"));
+// });
+
+// //#endregion
+
+app.listen(PORT, (err) => {
+  if (err) {
+    console.log(`ERROR: ${err}`);
+  } else {
+    console.log(`APP RUNNING at ${PORT} ✅`);
+  }
+});
