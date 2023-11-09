3612d2162fdc2592db06da471359929efccecd94
TeenagesWitch <hananmuhammadkemal@gmail.com>
2023-11-09 20:31:58 +0700

    Add getCartItems route to cartRoutes and implement getCartItems function in cartController

--- projects/server/src/routes/cartRoutes.js
+++ projects/server/src/routes/cartRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { addToCart, getCartItemCount } = require('../controller/cartController');
+const { addToCart, getCartItemCount, getCartItems } = require('../controller/cartController');
 const { validateToken } = require('../middleware/auth'); // Make sure to require the validateToken middleware
 
 // Route to handle adding items to the cart
@@ -8,5 +8,8 @@ router.post('/', validateToken, addToCart); // Include validateToken middleware
 
 // Route to get the count of items in the user's cart
 router.get('/items/count', validateToken, getCartItemCount); // Include validateToken middleware
+
+// Route to get the items in the user's cart
+router.get('/items', validateToken, getCartItems); // Include validateToken middleware
 
 module.exports = router;
