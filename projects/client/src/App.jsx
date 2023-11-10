da310009b25fd55ee0589f2949214e1841942c6c
anggaratriputra <anggaratriputra@gmail.com>
2023-11-10 09:44:10 +0700

    add validation route to render navbar

--- projects/client/src/App.jsx
+++ projects/client/src/App.jsx
@@ -1,20 +1,27 @@
 import { Route, Routes } from "react-router-dom";
 import { Homepage } from "./pages/Homepage";
 import ResetPassword from "./pages/ResetPassword";
-
 import { Profile } from "./pages/Profile";
 import { Address } from "./pages/Address";
 import { ChangePassword } from "./pages/ChangePassword";
 import { NotFound } from "./pages/NotFound";
 import Navigationbar from "./components/Navigationbar";
 
+import { useNavigate, useLocation } from "react-router-dom";
+
 export default function App() {
+  const location = useLocation();
+
+  // Hide Navigationbar for /reset-password route
+  const renderNavigationbar = location.pathname !== "/reset-password";
+
   return (
     <div>
-      <div className="shadow-md">
-        <Navigationbar />
-      </div>
-
+      {renderNavigationbar && (
+        <div className="shadow-md">
+          <Navigationbar />
+        </div>
+      )}
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/reset-password" element={<ResetPassword />} />
