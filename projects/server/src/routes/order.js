66dde26fc2277c7e21fdb4d3f589bed88d5b6eb9
fauzarizky <fauzasmg@gmail.com>
2023-11-16 13:03:31 +0700

    add filter, pagination & sort

--- projects/server/src/routes/order.js
+++ projects/server/src/routes/order.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const orderController = require("../controller/order");
 const { multerUpload } = require("../lib/multer");
 
-router.get("/",orderController.getAllOrder);
+router.get("/",orderController.getOrderLists);
 router.put("/:userId/:id",multerUpload.single("paymentProofImage"), orderController.paymentProof);
 
 module.exports = router;
