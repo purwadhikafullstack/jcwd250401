099923afed97d449f444b2dde368bf9cb2a3b700
anggaratriputra <anggaratriputra@gmail.com>
2023-11-20 20:13:25 +0700

    more spesific token malformed or expired

--- projects/server/src/routes/product.js
+++ projects/server/src/routes/product.js
@@ -7,10 +7,15 @@ const productValidator = require("../middleware/validation/product");
 const authMiddleware = require("../middleware/auth")
 
 router.post("/", authMiddleware.validateToken, authMiddleware.checkRoleSuperAdmin, multerUpload.array("productImages", 5), productValidator.productValidationRules, productValidator.applyProductValidation, productController.handleAddProduct);
+
 router.put("/:productId", authMiddleware.validateToken, authMiddleware.checkRoleSuperAdmin,  multerUpload.array("productImages", 5), productValidator.productValidationRules, productValidator.applyProductValidation, productController.handleUpdateProduct);
+
 router.get("/", authMiddleware.validateToken, productController.handleGetAllProducts)
+
 router.put("/archive/:productId", authMiddleware.validateToken, authMiddleware.checkRoleSuperAdmin, productController.handleArchiveProduct);
+
 router.put("/unarchive/:productId", authMiddleware.validateToken, authMiddleware.checkRoleSuperAdmin, productController.handleUnarchiveProduct);
+
 router.delete("/:productId", authMiddleware.validateToken, authMiddleware.checkRoleSuperAdmin, productController.handleDeleteProduct);
 
 module.exports = router;
