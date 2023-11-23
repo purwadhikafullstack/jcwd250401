5a44f6cabeeb7306350789ec60c2f0c21076d5dc
anggaratriputra <anggaratriputra@gmail.com>
2023-11-23 15:30:51 +0700

    fixing navbar

--- projects/client/src/api/products/getProducts.js
+++ projects/client/src/api/products/getProducts.js
@@ -1,27 +1,19 @@
 import api from "../../api";
 
-const getProducts = async ({
-  page = 1,
-  limit,
-  sort,
-  category,
-  search,
-  filterBy,
-  isArchived = false,
-} = {}) => {
+const getProducts = async ({ page = 1, limit, sort, category, search, filterBy, isArchived = false } = {}) => {
   try {
-    const url = `/product?page=${page}&isArchived=${isArchived}` +
-      (limit ? `&limit=${limit}` : '') +
-      (sort ? `&sort=${sort}` : '') +
-      (category ? `&category=${category}` : '') +
-      (search ? `&search=${search}` : '') +
-      (filterBy ? `&filterBy=${filterBy}` : '');
+    const url =
+      `/product?page=${page}&isArchived=${isArchived}` +
+      (limit ? `&limit=${limit}` : "") +
+      (sort ? `&sort=${sort}` : "") +
+      (category ? `&category=${category}` : "") +
+      (search ? `&search=${search}` : "") +
+      (filterBy ? `&filterBy=${filterBy}` : "");
 
     const response = await api.admin.get(url);
-    return response.data; 
-
+    return response.data;
   } catch (error) {
-    console.error('Error in useGetProduct:', error);
+    console.error("Error in useGetProduct:", error);
     throw error;
   }
 };
