7c6c299cab14e8fe39478ccf6fb69fbe91378bb8
anggaratriputra <anggaratriputra@gmail.com>
2023-12-07 09:52:34 +0700

    fix position checkout page

--- projects/client/src/pages/CheckoutPage.jsx
+++ projects/client/src/pages/CheckoutPage.jsx
@@ -24,18 +24,18 @@ function CheckoutPage() {
       <div className="px-6 lg:px-32 pt-4">
         <CartBreadcrumbs />
       </div>
-      <div className="container mx-auto px-6 lg:px-32 mt-4 flex flex-wrap md:flex-nowrap">
+      <div className="px-6 w-full lg:pl-32 mt-4 flex lg:flex-row flex-col justify-between">
         {/* Main content area with DeliveryOptions, adjusted for responsive width */}
-        <div className="w-full lg:flex-1 lg:max-w-lg xl:max-w-xl 2xl:max-w-2xl mb-6 md:mb-0">
+        <div className="w-full lg:flex-col">
           <DeliveryOptions />
           {!isPaymentOpen && (
-            <div className="mt-2 lg:mt-4 p-6 flex flex-col h-62 border rounded-md lg:w-[40vw]  bg-gray-200">
+            <div className="mt-2 lg:mt-4 p-6 flex flex-col h-62 border rounded-md lg:w-[48vw]  bg-gray-200">
               <h2 className="font-bold text-xl mb-2">2. Payment Method</h2>
             </div>
           )}
         </div>
         {/* Sidebar area with OrderSummary */}
-        <div className="w-full lg:w-1/3 xl:w-1/4">
+        <div className="w-full lg:px-6 flex justify-start lg:justify-center lg:h-64">
           <OrderSummary />
         </div>
       </div>
